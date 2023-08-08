bad3eced6b41879eb391f9359ccd3b08ca8bd83d
Tolga Simsek <tsi@david-bock.de>
2023-08-08 09:10:19 +0200

    Update api.ts

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,22 +1,20 @@
+import express from "express";
+import { Server } from "http";
 import serverless from "serverless-http";
+import { parseString } from "xml2js";
+import https from "https";
 
-import { Router } from "express";
-const express = require("express");
 const app = express();
-const { Server } = require("http");
 const server = Server(app);
-const path = require("path");
-const compression = require("compression");
 
-var parseString = require("xml2js").parseString;
-var https = require("https");
 app.use(express.urlencoded({ extended: false }));
 app.use(compression());
 app.use(express.json());
-const router = Router();
-/////////////////////////////// Change URL Below
 
-router.get("/api/xml", async (request, response) => {
+// Define router
+const router = express.Router();
+
+router.get("/xml", async (request, response) => {
     function xmlToJson(url, callback) {
         var req = https.get(url, function (res) {
             var xml = "";
@@ -47,14 +45,14 @@ router.get("/api/xml", async (request, response) => {
     xmlToJson(url, function (err, data) {
         console.log(data.spielplan.vst);
         if (err) {
-            return console.error(err);
+            return console.err(err);
         }
 
         response.json(data.spielplan);
     });
 });
 
-///////////////////////////////
-app.use("/api/", router);
+// Use the router
+app.use("/api", router);
 
 export const handler = serverless(app);
